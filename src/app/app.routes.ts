cf7ee96eccfa2a96cdfa5883ae68f18739a6b947
Carlos Ramirez <carlos@extrasistemas.com>
2025-04-01 00:25:54 -0600

    feat: add children components search and trending

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,22 @@ export const routes: Routes = [
   {
     path:  'dashboard',
     loadComponent: () => import('./gifts/pages/dashboard-page/dashboard-page.component'),
- },{
+    children:[
+      {
+        path:  'trending',
+        loadComponent: () => import('./gifts/pages/trending-page/trending-page.component'),
+      },
+      {
+        path:  'search',
+        loadComponent: () => import('./gifts/pages/search-page/search-page.component'),
+      },
+      {
+        path: '**',
+        redirectTo: 'trending',
+       }
+    ]
+ },
+ {
   path: '**',
   redirectTo: 'dashboard',
  }
