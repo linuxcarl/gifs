865f17ca87c56eb58ef86153f4e57cdd7968e36a
Carlos Ramirez <carlos@extrasistemas.com>
2025-04-01 10:32:54 -0600

    feat: add enviroments vars

--- src/app/gifts/components/side-menu/side-menu-header/side-menu-header.component.ts
+++ src/app/gifts/components/side-menu/side-menu-header/side-menu-header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'gifts-side-menu-header',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   templateUrl: './side-menu-header.component.html',
 })
 export default class SideMenuHeaderComponent {
-
+ envs= environment
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifts',
+  companyAlias: 'app',
+  companySlogan: 'Gifts for everyone',
+};
