d3fd326b26953724d577bdaf59b9adcaed75f0bf
Carlos Ramirez <carlos@extrasistemas.com>
2025-04-06 23:02:23 -0600

    feat: add components gift lists and items

---
+++ src/app/gifts/components/gift-list/gift-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+import { GiftListItemComponent } from './gift-list-item/gift-list-item.component';
+
+@Component({
+  selector: 'gift-list',
+  imports: [GiftListItemComponent],
+  templateUrl: './gift-list.component.html',
+})
+export class GiftListComponent {
+  gifs = input.required<string[]>();
+}
